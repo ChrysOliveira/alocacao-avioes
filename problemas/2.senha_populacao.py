e982cd43eea37f64d7c416f2f74d5cb329ac3f65
ErickLima <101227795+Erick141004@users.noreply.github.com>
2024-03-27 12:19:11 -0300

    Update 2.senha_populacao.py

--- problemas/2.senha_populacao.py
+++ problemas/2.senha_populacao.py
@@ -3,37 +3,36 @@ from copy import copy
 
 quantidade_max_avioes = 84
 #de 0 ate 24h em minutos pulando de 30 em 30 minutos
-horarios_possiveis_saida = [i for i in range(0, 1440, 30)]
-
+horarios_possiveis_saida = [i for i in range(60, 1440, 30)]
 
 def faz_lista_inicial():
     lista = []
 
     for i in range(84):
         if i in range(10):
-            lista.append([i, "SP", "RJ", 0, 60])
+            lista.append([i, "SP", "RJ", 0, 60, 0])
         elif i in range(10, 16):
-            lista.append([i, "SP", "BR", 0, 120])
+            lista.append([i, "SP", "BR", 0, 120, 0])
         elif i in range(16, 24):
-            lista.append([i, "SP", "BH", 0, 90])
+            lista.append([i, "SP", "BH", 0, 90, 0])
         elif i in range(24, 34):
-            lista.append([i, "RJ", "SP", 0, 60])
+            lista.append([i, "RJ", "SP", 0, 60, 0])
         elif i in range(34, 39):
-            lista.append([i, "RJ", "BR", 0, 120])
+            lista.append([i, "RJ", "BR", 0, 120, 0])
         elif i in range(39, 45):
-            lista.append([i, "RJ", "BH", 0, 90])
+            lista.append([i, "RJ", "BH", 0, 90, 0])
         elif i in range(45, 51):
-            lista.append([i, "BR", "SP", 0, 120])
+            lista.append([i, "BR", "SP", 0, 120, 0])
         elif i in range(51, 56):
-            lista.append([i, "BR", "RJ", 0, 120])
+            lista.append([i, "BR", "RJ", 0, 120, 0])
         elif i in range(56, 63):
-            lista.append([i, "BR", "BH", 0, 90])
+            lista.append([i, "BR", "BH", 0, 90, 0])
         elif i in range(63, 71):
-            lista.append([i, "BH", "SP", 0, 90])
+            lista.append([i, "BH", "SP", 0, 90, 0])
         elif i in range(71, 77):
-            lista.append([i, "BH", "RJ", 0, 90])
+            lista.append([i, "BH", "RJ", 0, 90, 0])
         elif i in range(77, 84):
-            lista.append([i, "BH", "BR", 0, 90])
+            lista.append([i, "BH", "BR", 0, 90, 0])
 
     return lista
 
@@ -46,20 +45,21 @@ def mutar(individuo):
 
         novo_horario = random.randint(0, len(horarios_possiveis_saida) - 1)
         alocacao[3] = horarios_possiveis_saida[novo_horario]
+        alocacao[5] = alocacao[3] + 30 + alocacao[4]
 
     return novo_individuo
 
 def fitness(individuo):
     punicao_total = 0
     avioes_visitados = set()
-    sorted(individuo, key=lambda alocacao: alocacao[3])
+    individuo = sorted(individuo, key=lambda alocacao: alocacao[3])
 
     for alocacao in individuo:
         individuo_copia_sem_atual = list(copy(individuo))
         individuo_copia_sem_atual.remove(alocacao)
 
         aviao = alocacao[0]
-        aviao_tempo_ocupado = [alocacao[3] - 60, alocacao[4] + 30]
+        aviao_tempo_ocupado = [alocacao[3] - 60, alocacao[5]]
 
         # verifica se a manutencao do mesmo dia
         if aviao_tempo_ocupado[0] < 0 or aviao_tempo_ocupado[0] > 1440:
@@ -69,9 +69,11 @@ def fitness(individuo):
         # valida se tem conflito de aviao nos voos -> mesmo aviao em dois voos diferentes no mesmo horario
         for prox_alocacao in individuo_copia_sem_atual:
             if prox_alocacao[0] == aviao:
-                prox_aviao_tempo_ocupado = [prox_alocacao[3] - 60, prox_alocacao[4] + 30]
+                prox_aviao_tempo_ocupado = [prox_alocacao[3] - 60, prox_alocacao[5]]
+                #or min(aviao_tempo_ocupado) > max(prox_aviao_tempo_ocupado)
 
-                if max(aviao_tempo_ocupado) < min(prox_aviao_tempo_ocupado) or min(aviao_tempo_ocupado) > max(prox_aviao_tempo_ocupado):
+                #trocamos o menor por maior
+                if max(aviao_tempo_ocupado) > min(prox_aviao_tempo_ocupado):
                     punicao_total = float("-inf")
                     break
                 elif alocacao[2] != prox_alocacao[1]:
@@ -90,7 +92,7 @@ def fitness(individuo):
 
             for prox_alocacao in individuo_copia_sem_atual:
                 if prox_alocacao[0] == aviao:
-                    prox_aviao_tempo_ocupado = [prox_alocacao[3] - 60, prox_alocacao[4] + 30]
+                    prox_aviao_tempo_ocupado = [prox_alocacao[3] - 60, prox_alocacao[5]]
                     tempo_alocacao_total += prox_aviao_tempo_ocupado[1] - prox_aviao_tempo_ocupado[0]
 
             tempo_parado = (1440 - tempo_alocacao_total) * -1
@@ -116,11 +118,12 @@ while True:
 
     geracoes += 1
 
-    if geracoes % 50 == 0:
-       print(populacao)
-       quantidade_max_avioes -= 1
+    if geracoes % 1000 == 0:
+        quantidade_max_avioes -= 1
+        ordenado_por_horario = sorted(populacao[0], key=lambda alocacao: (alocacao[0], alocacao[3]))
+        print(f"Populacao no momento: {ordenado_por_horario} \nQuantidade de avioes: {quantidade_max_avioes}\nFitness do melhor: {fitness(populacao[0])}\n\n")
     # critério de parada
-    if quantidade_max_avioes == 0:
+    if geracoes == 84000:
         melhor_individuo = populacao[0]
         print(f"Melhor solucao {sorted(melhor_individuo, key=lambda alocacao: alocacao[3])}")
         break
